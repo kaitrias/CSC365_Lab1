f1d55cbb775c127f163477824b073371d20a9520
ksunjara <ksunjara@calpoly.edu>
2019-09-23 16:56:54 -0700

    adding schoolsearch changes

---
+++ schoolsearch.py
@@ -0,0 +1,20 @@
+#user_input = input("Input a search command: ")   
+
+def parse_input(input_string):
+   parsed = input_string.split(' ')
+   return parsed
+def switch_cases()
+def main():
+   user_input = input("Input a search command: ")
+   parsed_input = parse_input(user_input)
+   
+#if user_input == 'S' or "Student":
+#   user_input = input("
+#   input_file = open("students.txt", "r")
+#   for line in input_file:
+#      print ("Line {:s}".format(line.rstrip()))
+#   input_file.close()
+#else:
+#   print ("Not Student")
+if __name__ == '__main__':
+   main()
