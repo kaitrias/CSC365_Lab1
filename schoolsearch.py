abad8b3f6b587f8c949676e95e37b96762f51e5f
ksunjara <ksunjara@calpoly.edu>
2019-09-24 16:18:58 -0700

    updating schoolsearch

--- schoolsearch.py
+++ schoolsearch.py
@@ -1,20 +1,33 @@
-#user_input = input("Input a search command: ")   
 
 def parse_input(input_string):
    parsed = input_string.split(' ')
    return parsed
-def switch_cases()
+
 def main():
-   user_input = input("Input a search command: ")
-   parsed_input = parse_input(user_input)
-   
-#if user_input == 'S' or "Student":
-#   user_input = input("
-#   input_file = open("students.txt", "r")
-#   for line in input_file:
-#      print ("Line {:s}".format(line.rstrip()))
-#   input_file.close()
-#else:
-#   print ("Not Student")
+   entry = "q"
+   while entry != "Q:" and  entry != "Quit:":
+      user_input = input ("Input a search command: ")
+      parsed_input = parse_input(user_input)
+      entry = parsed_input[0]
+      if (entry == "S:" or entry == "Student:"):
+         print ('S')
+         break
+      elif (entry == 'T' or entry == "Teacher"):
+         print ('T')
+         break
+      elif (entry == 'B' or entry == "Bus"):
+         print('B')
+         break
+      elif (entry == 'G' or entry == "Grade"):
+         print('G')
+         break
+      elif (entry == 'A' or entry == "Average"):
+         print('A')
+         break
+      elif (entry == 'I' or entry == "Info"):
+         print('I')
+         break
+   return
+
 if __name__ == '__main__':
    main()
